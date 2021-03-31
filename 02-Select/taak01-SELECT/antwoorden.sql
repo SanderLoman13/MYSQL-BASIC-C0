a7d1482d9c3839bbc2bf19e1d3a5506f5060ffab
Sander Loman <sander.loman@student.rocva.nl>
2021-03-31 14:46:00 +0200

    taak 2 af

--- 02-Select/taak01-SELECT/antwoorden.sql
+++ 02-Select/taak01-SELECT/antwoorden.sql
@@ -1,13 +1,767 @@
 -- Opdracht 1 - voorbeeld
 SELECT * FROM jaar2016
 -- Opdracht 2 
-
+SELECT * FROM jaar2015
 -- Opdracht 3
-
+Denmark
+Western Europe
+1
+7526
+Switzerland
+Western Europe
+2
+7509
+Iceland
+Western Europe
+3
+7501
+Norway
+Western Europe
+4
+7498
+Finland
+Western Europe
+5
+7413
+Canada
+North America
+6
+7404
+Netherlands
+Western Europe
+7
+7339
+New Zealand
+Australia and New Zealand
+8
+7334
+Australia
+Australia and New Zealand
+9
+7313
+Sweden
+Western Europe
+10
+7291
+Israel
+Middle East and Northern Africa
+11
+7267
+Austria
+Western Europe
+12
+7119
+United States
+North America
+13
+7104
+Costa Rica
+Latin America and Caribbean
+14
+7087
+Puerto Rico
+Latin America and Caribbean
+15
+7039
+Germany
+Western Europe
+16
+6994
+Brazil
+Latin America and Caribbean
+17
+6952
+Belgium
+Western Europe
+18
+6929
+Ireland
+Western Europe
+19
+6907
+Luxembourg
+Western Europe
+20
+6871
+Mexico
+Latin America and Caribbean
+21
+6778
+Singapore
+Southeastern Asia
+22
+6739
+United Kingdom
+Western Europe
+23
+6725
+Chile
+Latin America and Caribbean
+24
+6705
+Panama
+Latin America and Caribbean
+25
+6701
+Argentina
+Latin America and Caribbean
+26
+665
+Czech Republic
+Central and Eastern Europe
+27
+6596
+United Arab Emirates
+Middle East and Northern Africa
+28
+6573
+Uruguay
+Latin America and Caribbean
+29
+6545
+Malta
+Western Europe
+30
+6488
+Colombia
+Latin America and Caribbean
+31
+6481
+France
+Western Europe
+32
+6478
+Thailand
+Southeastern Asia
+33
+6474
+Saudi Arabia
+Middle East and Northern Africa
+34
+6379
+Taiwan
+Eastern Asia
+34
+6379
+Qatar
+Middle East and Northern Africa
+36
+6375
+Spain
+Western Europe
+37
+6361
+Algeria
+Middle East and Northern Africa
+38
+6355
+Guatemala
+Latin America and Caribbean
+39
+6324
+Suriname
+Latin America and Caribbean
+40
+6269
+Kuwait
+Middle East and Northern Africa
+41
+6239
+Bahrain
+Middle East and Northern Africa
+42
+6218
+Trinidad and Tobago
+Latin America and Caribbean
+43
+6168
+Venezuela
+Latin America and Caribbean
+44
+6084
+Slovakia
+Central and Eastern Europe
+45
+6078
+El Salvador
+Latin America and Caribbean
+46
+6068
+Malaysia
+Southeastern Asia
+47
+6005
+Nicaragua
+Latin America and Caribbean
+48
+5992
+Uzbekistan
+Central and Eastern Europe
+49
+5987
+Italy
+Western Europe
+50
+5977
+Ecuador
+Latin America and Caribbean
+51
+5976
+Belize
+Latin America and Caribbean
+52
+5956
+Japan
+Eastern Asia
+53
+5921
+Kazakhstan
+Central and Eastern Europe
+54
+5919
+Moldova
+Central and Eastern Europe
+55
+5897
+Russia
+Central and Eastern Europe
+56
+5856
+Poland
+Central and Eastern Europe
+57
+5835
+South Korea
+Eastern Asia
+57
+5835
+Bolivia
+Latin America and Caribbean
+59
+5822
+Lithuania
+Central and Eastern Europe
+60
+5813
+Belarus
+Central and Eastern Europe
+61
+5802
+North Cyprus
+Western Europe
+62
+5771
+Slovenia
+Central and Eastern Europe
+63
+5768
+Peru
+Latin America and Caribbean
+64
+5743
+Turkmenistan
+Central and Eastern Europe
+65
+5658
+Mauritius
+Sub-Saharan Africa
+66
+5648
+Libya
+Middle East and Northern Africa
+67
+5615
+Latvia
+Central and Eastern Europe
+68
+556
+Cyprus
+Western Europe
+69
+5546
+Paraguay
+Latin America and Caribbean
+70
+5538
+Romania
+Central and Eastern Europe
+71
+5528
+Estonia
+Central and Eastern Europe
+72
+5517
+Jamaica
+Latin America and Caribbean
+73
+551
+Croatia
+Central and Eastern Europe
+74
+5488
+Hong Kong
+Eastern Asia
+75
+5458
+Somalia
+Sub-Saharan Africa
+76
+544
+Kosovo
+Central and Eastern Europe
+77
+5401
+Turkey
+Middle East and Northern Africa
+78
+5389
+Indonesia
+Southeastern Asia
+79
+5314
+Jordan
+Middle East and Northern Africa
+80
+5303
+Azerbaijan
+Central and Eastern Europe
+81
+5291
+Philippines
+Southeastern Asia
+82
+5279
+China
+Eastern Asia
+83
+5245
+Bhutan
+Southern Asia
+84
+5196
+Kyrgyzstan
+Central and Eastern Europe
+85
+5185
+Serbia
+Central and Eastern Europe
+86
+5177
+Bosnia and Herzegovina
+Central and Eastern Europe
+87
+5163
+Montenegro
+Central and Eastern Europe
+88
+5161
+Dominican Republic
+Latin America and Caribbean
+89
+5155
+Morocco
+Middle East and Northern Africa
+90
+5151
+Hungary
+Central and Eastern Europe
+91
+5145
+Pakistan
+Southern Asia
+92
+5132
+Lebanon
+Middle East and Northern Africa
+93
+5129
+Portugal
+Western Europe
+94
+5123
+Macedonia
+Central and Eastern Europe
+95
+5121
+Vietnam
+Southeastern Asia
+96
+5061
+Somaliland Region
+Sub-Saharan Africa
+97
+5057
+Tunisia
+Middle East and Northern Africa
+98
+5045
+Greece
+Western Europe
+99
+5033
+Tajikistan
+Central and Eastern Europe
+100
+4996
+country
+region
+rank
+score
+ 
+Mongolia
+Eastern Asia
+101
+4907
+Laos
+Southeastern Asia
+102
+4876
+Nigeria
+Sub-Saharan Africa
+103
+4875
+Honduras
+Latin America and Caribbean
+104
+4871
+Iran
+Middle East and Northern Africa
+105
+4813
+Zambia
+Sub-Saharan Africa
+106
+4795
+Nepal
+Southern Asia
+107
+4793
+Palestinian Territories
+Middle East and Northern Africa
+108
+4754
+Albania
+Central and Eastern Europe
+109
+4655
+Bangladesh
+Southern Asia
+110
+4643
+Sierra Leone
+Sub-Saharan Africa
+111
+4635
+Iraq
+Middle East and Northern Africa
+112
+4575
+Namibia
+Sub-Saharan Africa
+113
+4574
+Cameroon
+Sub-Saharan Africa
+114
+4513
+Ethiopia
+Sub-Saharan Africa
+115
+4508
+South Africa
+Sub-Saharan Africa
+116
+4459
+Sri Lanka
+Southern Asia
+117
+4415
+India
+Southern Asia
+118
+4404
+Myanmar
+Southeastern Asia
+119
+4395
+Egypt
+Middle East and Northern Africa
+120
+4362
+Armenia
+Central and Eastern Europe
+121
+436
+Kenya
+Sub-Saharan Africa
+122
+4356
+Ukraine
+Central and Eastern Europe
+123
+4324
+Ghana
+Sub-Saharan Africa
+124
+4276
+Congo (Kinshasa)
+Sub-Saharan Africa
+125
+4272
+Georgia
+Central and Eastern Europe
+126
+4252
+Congo (Brazzaville)
+Sub-Saharan Africa
+127
+4236
+Senegal
+Sub-Saharan Africa
+128
+4219
+Bulgaria
+Central and Eastern Europe
+129
+4217
+Mauritania
+Sub-Saharan Africa
+130
+4201
+Zimbabwe
+Sub-Saharan Africa
+131
+4193
+Malawi
+Sub-Saharan Africa
+132
+4156
+Sudan
+Sub-Saharan Africa
+133
+4139
+Gabon
+Sub-Saharan Africa
+134
+4121
+Mali
+Sub-Saharan Africa
+135
+4073
+Haiti
+Latin America and Caribbean
+136
+4028
+Botswana
+Sub-Saharan Africa
+137
+3974
+Comoros
+Sub-Saharan Africa
+138
+3956
+Ivory Coast
+Sub-Saharan Africa
+139
+3916
+Cambodia
+Southeastern Asia
+140
+3907
+Angola
+Sub-Saharan Africa
+141
+3866
+Niger
+Sub-Saharan Africa
+142
+3856
+South Sudan
+Sub-Saharan Africa
+143
+3832
+Chad
+Sub-Saharan Africa
+144
+3763
+Burkina Faso
+Sub-Saharan Africa
+145
+3739
+Uganda
+Sub-Saharan Africa
+145
+3739
+Yemen
+Middle East and Northern Africa
+147
+3724
+Madagascar
+Sub-Saharan Africa
+148
+3695
+Tanzania
+Sub-Saharan Africa
+149
+3666
+Liberia
+Sub-Saharan Africa
+150
+3622
+Guinea
+Sub-Saharan Africa
+151
+3607
+Rwanda
+Sub-Saharan Africa
+152
+3515
+Benin
+Sub-Saharan Africa
+153
+3484
+Afghanistan
+Southern Asia
+154
+336
+Togo
+Sub-Saharan Africa
+155
+3303
+Syria
+Middle East and Northern Africa
+156
+3069
+Burundi
+Sub-Saharan Africa
+157
+2905
 -- Opdracht 4
-
+Western Europe
+Western Europe
+Western Europe
+Western Europe
+North America
+Western Europe
+Western Europe
+Western Europe
+Australia and New Zealand
+Australia and New Zealand
+Middle East and Northern Africa
+Latin America and Caribbean
+Western Europe
+Latin America and Caribbean
+North America
+Latin America and Caribbean
+Western Europe
+Western Europe
+Western Europe
+Middle East and Northern Africa
+Western Europe
+Middle East and Northern Africa
+Latin America and Caribbean
+Southeastern Asia
+Latin America and Caribbean
 -- Opdracht 5
-
+Western Europe
+7587
+Western Europe
+7561
+Western Europe
+7527
+Western Europe
+7522
+North America
+7427
+Western Europe
+7406
+Western Europe
+7378
+Western Europe
+7364
+Australia and New Zealand
+7286
+Australia and New Zealand
+7284
+Middle East and Northern Africa
+7278
+Latin America and Caribbean
+7226
+Western Europe
+72
+Latin America and Caribbean
+7187
+North America
+7119
+Latin America and Caribbean
+6983
+Western Europe
+6946
+Western Europe
+694
+Western Europe
+6937
+Middle East and Northern Africa
+6901
+Western Europe
+6867
+Middle East and Northern Africa
+6853
+Latin America and Caribbean
+681
+Southeastern Asia
+6798
+Latin America and Caribbean
+6786
 -- Opdracht 6
+7526
+1
+7509
+2
+7501
+3
+7498
+4
+7413
+5
+7404
+6
+7339
+7
+7334
+8
+7313
+9
+7291
+10
+7267
+11
+7119
+12
+7104
+13
+7087
+14
+7039
+15
+6994
+16
+6952
+17
+6929
+18
+6907
+19
+6871
+20
+6778
+21
+6739
+22
+6725
+23
+6705
+24
+6701
+25
 
--- Opdracht 7
