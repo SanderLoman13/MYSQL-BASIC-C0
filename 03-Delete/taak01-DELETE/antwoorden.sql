ee512333fb9fadcfd24f13363452a344f4681156
Sander Loman <sander.loman@student.rocva.nl>
2021-04-15 11:14:45 +0200

    taak 7 af

--- 03-Delete/taak01-DELETE/antwoorden.sql
+++ 03-Delete/taak01-DELETE/antwoorden.sql
@@ -1,9 +1,10 @@
 -- Opdracht 1 
-
+DELETE FROM `players` WHERE name = "David Sylva"
 -- Opdracht 2 
-
+DELETE players FROM `players` WHERE club= "Willem II"
 -- Opdracht 3
-
+DELETE players FROM `players` WHERE nationality = "belgium" AND club = "FC Barcelona"
 -- Opdracht 4
-
+DELETE players FROM `players` WHERE age >= 38
 -- Opdracht 5
+DELETE players FROM `players` WHERE id = 167905 AND 169595
